5b29dfbafa2ddf9e9f6bca1b5af13a65df055e9e
Karthi <karthiamar07@gmail.com>
2026-08-12 12:03:07 +0530

    Created the rest of tf file for dev

---
+++ environments/dev/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+    description = "GCP project id"
+    type = string
+}
+
+variable "region" {
+    description = " GCP region"
+    type = string
+    default = "us-central1"
+}
+
+variable "zone" {
+    description = " GCP zone"
+    type = string
+    default = "us-central1-a"
+}
+
+variable "env" {
+    description = " Environment name "
+    type = string
+    default = "dev"
+}
+
+variable "subnet_cidr" {
+    description = "Range of subnet "
+    type = string
+    default = "10.0.1.0/24"
+}
